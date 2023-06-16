3459058a79a7c3d787ab8beb7db5e2727872339e
minheebaek <105588896+minheebaek@users.noreply.github.com>
2023-06-16 23:51:04 +0900

    Create boj2587.java

---
+++ boj2587.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.Arrays;
+
+public class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int sum =0;
+        int[] arr = new int[5];
+        for (int i = 0; i < arr.length; i++) {
+           arr[i]=Integer.parseInt(br.readLine());
+            sum+=arr[i];
+        }
+        Arrays.sort(arr);
+        System.out.println(sum/5);
+        System.out.println(arr[2]);
+
+    }
+}
