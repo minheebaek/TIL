db70e2445728c674aa51dd90dfe6ad7db3fe04c3
minheebaek <105588896+minheebaek@users.noreply.github.com>
2023-06-17 12:08:40 +0900

    Create boj2750.java

---
+++ boj2750.java
@@ -0,0 +1,26 @@
+import java.io.*;
+public class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        for(int i=0; i<N; i++){
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        int tmp=0;
+        for(int i=0; i<arr.length; i++){
+            for(int j=0; j<arr.length-1-i; j++){
+                if(arr[j]>arr[j+1]){
+                    tmp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=tmp;
+                }
+            }
+        }
+
+        for(int i=0; i<arr.length; i++){
+            System.out.println(arr[i]);
+        }
+    }
+}
